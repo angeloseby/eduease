828fa9b385c2c585eaca433a97ecc65d1bf06bf0
Angelo Seby <93852988+angeloseby@users.noreply.github.com>
2023-04-13 01:02:52 +0530

    First Host File
    Only test home page implemented

---
+++ main.js
@@ -0,0 +1,24 @@
+// change navbar style on scroll
+
+window.addEventListener('scroll',()=>{
+    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY>0);
+});
+
+// show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click',()=>{
+        faq.classList.toggle('open');
+        
+
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className ==='uil uil-plus' ){
+            icon.className = 'uil uil-minus'
+        }else{
+            icon.className ='uil uil-plus'
+        }
+
+    })
+});
